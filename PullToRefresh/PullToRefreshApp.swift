cf18c91b51e02dc2f8751913b360bd2a86a317b5
Max <maxim.macari.dam@gmail.com>
2020-11-06 14:26:22 +0100

    Initial Commit

---
+++ PullToRefresh/PullToRefreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  PullToRefreshApp.swift
+//  PullToRefresh
+//
+//  Created by Maxim Macari on 06/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct PullToRefreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
